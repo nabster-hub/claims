ea7a0ceaf763704fa69cf92bbf67ffa1b6b6307d
Naimanshalov Ilias <mail@nabster.name>
2025-03-07 05:45:08 +0500

    add time on claims list

--- resources/views/claim/allClaims.blade.php
+++ resources/views/claim/allClaims.blade.php
@@ -78,7 +78,9 @@
                                 <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                     <div class="flex items-center">
                                         <div class="ml-4">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">{{ $claim->full_name }}</div>
+                                            <div class="text-sm leading-5 font-medium text-gray-900">{{ $claim->full_name }}
+                                                <span class="text-[10px] block text-gray-400">{{$claim->created_at->format('d.m.y H:i:s')}}</span>
+                                            </div>
                                         </div>
                                     </div>
                                 </td>
